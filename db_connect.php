010b54d174cad7ccf3aa5b6693d63015bbe3ef0c
venzpagaduan <venzpagaduan2@gmail.com>
2025-04-18 19:25:25 +0800

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -3,10 +3,13 @@ $host = 'sql12.freesqldatabase.com';
 $db = 'sql12773707';
 $user = 'sql12773707';
 $pass = 'cCsEigKhzd'; // Leave blank for XAMPP default
+$port = 3306;
 $charset = 'utf8mb4';
 
+
+
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$db;charset=$charset", $user, $pass);
+    $pdo = new PDO("mysql:host=$host;dbname=$db;charset=$charset", $user, $pass, $port);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Connection failed: " . $e->getMessage());
